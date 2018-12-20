a6b181cd6624ec9db7d61f59086c8a7efddafe54
briksing <briksing@yandex.ru>
2018-12-20 13:57:32 +0500

    Fix Realtor and Main Controllers

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,17 +3,50 @@
 namespace App\Http\Controllers;
 
 use App\Realty;
+use App\Object;
+use App\House;
+use App\Property;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function GetAllRealty()
+    public function GetAllRealty(Request $request)
     {
-       $realties = Realty::all();
+       $realties = Realty::get();
+       $isRequestExist = count($request->all());
+
+
+       if($isRequestExist) {
+         $realties = $request->property ? $realties->where('property_id', $request->property) : $realties;
+         $realties = $request->object ? $realties->where('object_id', $request->object) : $realties;
+         $realties = $request->house ? $realties->where('house_id', $request->house) : $realties;
+         $realties = $request->floor ? $realties->where('floor', $request->floor) : $realties;
+         $realties = $request->numberOfRooms ? $realties->where('numberOfRooms', $request->numberOfRooms) : $realties;
+         $realties = $request->price ? $realties->where('price', "<", $request->price) : $realties;
+       }
+
+
+       $objects = Object::all();
+       $houses = House::all();
+       $properties = Property::all();
+
+       $numberOfFloors = [];
+       for ($i=1; $i <= 18; $i++) {
+         $numberOfFloors[] = $i;
+       }
 
        return view('main', [
-         'realties' => $realties
+         'realties' => $realties,
+         'objects' => $objects,
+         'houses' => $houses,
+         'properties' => $properties,
+         'floors' => $numberOfFloors
        ]);
+    }
+
+    public function GetAllObjects()
+    {
+
     }
 
 }
--- app/Http/Controllers/RealtorController.php
+++ app/Http/Controllers/RealtorController.php
@@ -3,8 +3,93 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use App\Client;
+use App\Deal;
+use App\Realtor;
+use App\Realty;
+use App\Property;
+use App\Object;
+use App\House;
+use App\Service;
+use Gate;
+use Auth;
 
 class RealtorController extends Controller
 {
-    //
+    public function GetClients()
+    {
+        if (Gate::denies('realtor')) {
+          return view('errors.403');
+        }
+
+        $clients = Client::all();
+
+        // $user = Auth::user();
+        // $realtor = $user->realtor;
+        // $deals = $realtor->deals;
+        // $clients = [];
+        //
+        // foreach ($deals as $deal) {
+        //   $client = $deal->realty->client;
+        //   $clients[] = $client;
+        // }
+
+        return view('realtorClients', [
+          'clients' => $clients
+        ]);
+    }
+
+    public function GetDeals()
+    {
+        if (Gate::denies('realtor')) {
+          return view('errors.403');
+        }
+
+        $user = Auth::user();
+        $realtor = $user->realtor;
+        $deals = $realtor->deals;
+
+        return view('realtorDeals', [
+          'deals' => $deals
+        ]);
+    }
+
+    public function GetAddFormInfo()
+    {
+        if (Gate::denies('realtor')) {
+          return view('errors.403');
+        }
+
+        $services = Service::all();
+        $realties = Realty::all();
+
+        return view('realtorAddForm', [
+          'services' => $services,
+          'realties' => $realties
+        ]);
+    }
+
+    public function AddDeal(Request $request)
+    {
+        if (Gate::denies('realtor')) {
+          return view('errors.403');
+        }
+        if(!!$request) {
+          $user = Auth::user();
+          $realtor = $user->realtor;
+          $realtor->deals()->create($request->all());
+        }
+
+        return $this->GetDeals();
+    }
+
+    public function GetMenu()
+    {
+      if (Gate::denies('realtor')) {
+        return view('errors.403');
+      }
+
+      return view('realtorMenu');
+    }
 }
